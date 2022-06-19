cc12553ccafc9213dd2b8081b76d3aba8bffd7d4
Onyash <onyashed@gmail.com>
2022-06-19 04:47:08 +0300

    initial commit

---
+++ test/unit_test.dart
@@ -0,0 +1,19 @@
+// This is an example unit test.
+//
+// A unit test tests a single function, method, or class. To learn more about
+// writing unit tests, visit
+// https://flutter.dev/docs/cookbook/testing/unit/introduction
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:bristol_exchange/main.dart';
+
+void main() {
+  group('Plus Operator', () {
+    test('should add two numbers together', () {
+      expect(1 + 1, 2);
+    });
+  });
+}
